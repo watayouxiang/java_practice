b2e0e25606bc9fb3cc9c2d4f8130bf724eecdedc
watayouxiang <watayouxiang@qq.com>
2023-03-08 14:07:49 +0800

    .

--- src/main/java/com/watayouxiang/myjava/utils/InvoiceUtils.java
+++ src/main/java/com/watayouxiang/myjava/utils/InvoiceUtils.java
@@ -11,9 +11,9 @@ import java.util.regex.Pattern;
 public class InvoiceUtils {
 
     /**
-     * 统计文件夹中的发票总金额
+     * 统计文件夹中的发票总额
      * <p>
-     * 遍历目录，该目录下没有子目录，内含如下文件：
+     * 遍历目录，该目录下没有子目录，内含如下发票：
      * 60.ofd
      * 68.14.pdf
      * 69.pdf
@@ -37,28 +37,21 @@ public class InvoiceUtils {
             throw new RuntimeException(file.getAbsolutePath() + "：无法访问");
         }
         if (files.length == 0) {
-            throw new RuntimeException(file.getAbsolutePath() + "：没有文件");
+            throw new RuntimeException(file.getAbsolutePath() + "：没有发票");
         }
         double total = 0;
         int count = 0;
+        Pattern pattern = Pattern.compile("(^\\d+\\.\\d+)|(^\\d+)");
         for (File f : files) {
-            // 60.ofd
-            // 68.14.pdf
-            // 69.pdf
-            // 74.2.png
-            // 76.9.pdf
-            // 200-.jpg
-            // 221.9.pdf
             String name = f.getName();
-            Matcher matcher = Pattern.compile("(^\\d+\\.\\d+)|(^\\d+)").matcher(name);
+            Matcher matcher = pattern.matcher(name);
             if (!matcher.find()) {
                 throw new RuntimeException(f.getAbsolutePath() + "：该发票无金额");
             }
-            String group = matcher.group();
-            double cny = Double.parseDouble(group);
+            double cny = Double.parseDouble(matcher.group());
             total += cny;
             count++;
-            // System.out.println(name + " --> " + cny);
+            System.out.println(name + " --> " + cny);
         }
         System.out.println(count + "张发票，总金额为：" + total + "元");
     }
