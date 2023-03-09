3f8d3c88c026e898d3615682045e124e59e320d4
watayouxiang <watayouxiang@qq.com>
2023-03-09 21:13:23 +0800

    .

--- src/main/java/com/watayouxiang/myjava/thread/singleton/Singleton7.java
+++ src/main/java/com/watayouxiang/myjava/thread/singleton/Singleton7.java
@@ -1,7 +1,7 @@
 package com.watayouxiang.myjava.thread.singleton;
 
 /**
- * 描述：     静态内部类方式，可用
+ * 描述：     静态内部类方式（推荐用）（懒汉式）
  */
 public class Singleton7 {
 
@@ -9,7 +9,7 @@ public class Singleton7 {
     }
 
     private static class SingletonInstance {
-
+        // 因为是内部类，所以不会被立马创建，只有当被使用时才会被创建
         private static final Singleton7 INSTANCE = new Singleton7();
     }
 
