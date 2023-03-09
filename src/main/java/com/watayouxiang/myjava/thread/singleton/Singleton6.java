b52ebc7730db0b1ccd92d2ffc6a4be5c0fc587b2
watayouxiang <watayouxiang@qq.com>
2023-03-09 21:04:54 +0800

    .

--- src/main/java/com/watayouxiang/myjava/thread/singleton/Singleton6.java
+++ src/main/java/com/watayouxiang/myjava/thread/singleton/Singleton6.java
@@ -1,10 +1,13 @@
 package com.watayouxiang.myjava.thread.singleton;
 
 /**
- * 描述：     双重检查（推荐面试使用）
+ * 描述：     双重检查（推荐面试使用）（线程安全）
  */
 public class Singleton6 {
 
+    // 这里必须要用 volatile 修饰
+    // 因为新建对象 instance = new Singleton6(); 实际上有3个步骤
+    // 如果不用 volatile 修饰，那么假如把 新建对象的3个步骤 进行指令重排序，就会出现安全问题
     private volatile static Singleton6 instance;
 
     private Singleton6() {
