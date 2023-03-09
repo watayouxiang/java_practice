bab4611c94a6e4a30d145f62912faddb4f02807f
watayouxiang <watayouxiang@qq.com>
2023-03-09 21:21:27 +0800

    .

--- src/main/java/com/watayouxiang/myjava/thread/singleton/Singleton8.java
+++ src/main/java/com/watayouxiang/myjava/thread/singleton/Singleton8.java
@@ -2,6 +2,10 @@ package com.watayouxiang.myjava.thread.singleton;
 
 /**
  * 描述：     枚举单例（线程安全）（推荐用）
+ * <p>
+ * 1、写法简单
+ * 2、线程安全有保证、懒加载
+ * 3、避免反序列化破坏单例（避免被反射暴露）
  */
 public enum Singleton8 {
     INSTANCE;
