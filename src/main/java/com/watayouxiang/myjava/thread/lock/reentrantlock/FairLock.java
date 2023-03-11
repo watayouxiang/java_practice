a496e6df1b7d546f5c0157459a990261a8dd394a
watayouxiang <watayouxiang@qq.com>
2023-03-11 22:00:50 +0800

    .

--- src/main/java/com/watayouxiang/myjava/thread/lock/reentrantlock/FairLock.java
+++ src/main/java/com/watayouxiang/myjava/thread/lock/reentrantlock/FairLock.java
@@ -40,30 +40,22 @@ class Job implements Runnable {
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName() + "开始打印");
-        printQueue.printJob(new Object());
+        printQueue.printJob("第一份文件");
+        printQueue.printJob("第二份文件");
         System.out.println(Thread.currentThread().getName() + "开始完毕");
     }
 }
 
 class PrintQueue {
-    private Lock queueLock = new ReentrantLock(true);
+
+    // 这里演示公平和非公平锁的差异
+    private Lock queueLock = new ReentrantLock(false);
 
     public void printJob(Object document) {
         queueLock.lock();
         try {
             int duration = new Random().nextInt(10) + 1;
-            System.out.println(Thread.currentThread().getName() + "正在打印，需要" + duration + "秒");
-            Thread.sleep(duration * 1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            queueLock.unlock();
-        }
-
-        queueLock.lock();
-        try {
-            int duration = new Random().nextInt(10) + 1;
-            System.out.println(Thread.currentThread().getName() + "正在打印，需要" + duration + "秒");
+            System.out.println(Thread.currentThread().getName() + "正在打印" + document + "，需要" + duration + "秒");
             Thread.sleep(duration * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
