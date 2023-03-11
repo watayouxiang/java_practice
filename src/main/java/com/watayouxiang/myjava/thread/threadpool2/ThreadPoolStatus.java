352ad494227f3d6a077d31ab442d9fa8d1501972
watayouxiang <watayouxiang@qq.com>
2023-03-11 18:11:56 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/threadpool2/ThreadPoolStatus.java
@@ -0,0 +1,17 @@
+package com.watayouxiang.myjava.thread.threadpool2;
+
+/**
+ 线程池状态：
+
+ RUNNING        接受新任务并处理排队任务
+ SHUTDOWN       不接受新任务，但处理排队任务
+ STOP           不接受新任务，也不处理排队任务，并中断正在运行的任务
+ TIDYING        中文是整洁的意思，理解了中文就很容易理解这个状态了：所有任务都已终止，workerCount为零时，
+                线程会转换到TIDYING状态，并将运行terminate()钩子方法。
+ TERMINATED     terminate()运行完成
+
+
+
+ */
+public class ThreadPoolStatus {
+}
