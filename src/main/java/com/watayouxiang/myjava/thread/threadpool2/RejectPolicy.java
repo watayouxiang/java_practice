b69f19a3d80ebe58a022941e749422216a7226e1
watayouxiang <watayouxiang@qq.com>
2023-03-11 17:17:00 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/threadpool2/RejectPolicy.java
@@ -0,0 +1,12 @@
+package com.watayouxiang.myjava.thread.threadpool2;
+
+/**
+AbortPolicy            抛出异常
+DiscardPolicy          把任务丢弃
+DiscardOldestPolicy    丢弃最老的任务
+CallerRunsPolicy       把任务丢给 "提交任务的人"
+
+ */
+public class RejectPolicy {
+
+}
