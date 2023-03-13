904be32ffff80f4df03b04070b87f2b037e9e9d1
watayouxiang <watayouxiang@qq.com>
2023-03-13 22:16:51 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/immutable/FinalStringDemo1.java
@@ -0,0 +1,19 @@
+package com.watayouxiang.myjava.thread.immutable;
+
+/**
+ * author：wangtao
+ * email：watayouixang@qq.com
+ * time：2023/3/13
+ * description：
+ */
+public class FinalStringDemo1 {
+    public static void main(String[] args) {
+        String a = "wukong2";
+        final String b = "wukong";
+        String d = "wukong";
+        String c = b + 2;
+        String e = d + 2;
+        System.out.println((a == c));
+        System.out.println((a == e));
+    }
+}
