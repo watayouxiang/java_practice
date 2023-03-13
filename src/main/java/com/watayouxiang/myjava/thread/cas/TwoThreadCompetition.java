30fe6681ca5bb92e066ce7f5956890df59af7f76
watayouxiang <watayouxiang@qq.com>
2023-03-13 21:36:17 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/cas/TwoThreadCompetition.java
@@ -0,0 +1,37 @@
+package com.watayouxiang.myjava.thread.cas;
+
+/**
+ * author：wangtao
+ * email：watayouixang@qq.com
+ * time：2023/3/13
+ * description：
+ */
+public class TwoThreadCompetition implements Runnable {
+
+    private volatile int value;
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if (oldValue == expectedValue) {
+            value = newValue;
+        }
+        return oldValue;
+    }
+
+    @Override
+    public void run() {
+        compareAndSwap(0, 1);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TwoThreadCompetition r = new TwoThreadCompetition();
+        r.value = 0;
+        Thread thread1 = new Thread(r, "线程1");
+        Thread thread2 = new Thread(r, "线程2");
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(r.value);
+    }
+}
