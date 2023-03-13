f81496a0264a6c589a270773464bb140144b614a
watayouxiang <watayouxiang@qq.com>
2023-03-13 22:12:13 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/immutable/StackConfinement.java
@@ -0,0 +1,39 @@
+package com.watayouxiang.myjava.thread.immutable;
+
+/**
+ * author：wangtao
+ * email：watayouixang@qq.com
+ * time：2023/3/13
+ * description：演示栈封闭的两种情况，基本变量和对象
+ * 先演示线程争抢带来错误结果，然后把变量放到方法内，情况变了
+ */
+public class StackConfinement implements Runnable {
+    int index = 0;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            index++;
+        }
+        inThread();
+    }
+
+    public void inThread() {
+        int neverGoOut = 0;
+        for (int i = 0; i < 10000; i++) {
+            neverGoOut++;
+        }
+        System.out.println("栈内保护的数字是线程安全的：" + neverGoOut);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        StackConfinement r = new StackConfinement();
+        Thread thread1 = new Thread(r);
+        Thread thread2 = new Thread(r);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(r.index);
+    }
+}
