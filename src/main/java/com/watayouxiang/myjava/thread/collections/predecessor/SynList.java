67b990d1172e0c78157579321fb7ed222e026ef0
watayouxiang <watayouxiang@qq.com>
2023-03-14 10:30:24 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/collections/predecessor/SynList.java
@@ -0,0 +1,19 @@
+package com.watayouxiang.myjava.thread.collections.predecessor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * author：wangtao
+ * email：watayouixang@qq.com
+ * time：2023/3/14
+ * description：演示Collections.synchronizedList(new ArrayList<E>())
+ */
+public class SynList {
+    public static void main(String[] args) {
+        List<Integer> list = Collections.synchronizedList(new ArrayList<Integer>());
+        list.add(5);
+        System.out.println(list.get(0));
+    }
+}
