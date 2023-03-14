0bfb94e90ba1b3bfd9fb17140bb2ab2d132cfe2b
watayouxiang <watayouxiang@qq.com>
2023-03-14 16:30:09 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/flowcontrol/condition/ConditionDemo1.java
@@ -0,0 +1,57 @@
+package com.watayouxiang.myjava.thread.flowcontrol.condition;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * author：wangtao
+ * email：watayouixang@qq.com
+ * time：2023/3/14
+ * description：
+ */
+public class ConditionDemo1 {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    void method1() throws InterruptedException {
+        lock.lock();
+        try {
+            System.out.println("条件不满足，开始await");
+            condition.await();
+            System.out.println("条件满足了，开始执行后续的任务");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    void method2() throws InterruptedException {
+        lock.lock();
+        try {
+            System.out.println("准备工作完成，唤醒其他的线程");
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConditionDemo1 demo1 = new ConditionDemo1();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    demo1.method2();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        try {
+            demo1.method1();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
