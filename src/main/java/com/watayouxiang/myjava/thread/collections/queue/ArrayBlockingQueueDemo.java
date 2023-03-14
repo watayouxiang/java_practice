93c77e5a6ab7f4c7af4322187b0e097a3d6685c4
watayouxiang <watayouxiang@qq.com>
2023-03-14 15:27:38 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/collections/queue/ArrayBlockingQueueDemo.java
@@ -0,0 +1,74 @@
+package com.watayouxiang.myjava.thread.collections.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * author：wangtao
+ * email：watayouixang@qq.com
+ * time：2023/3/14
+ * description：ArrayBlockingQueue的基本使用
+ */
+public class ArrayBlockingQueueDemo {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(3);
+        Interviewer r1 = new Interviewer(queue);
+        Consumer r2 = new Consumer(queue);
+        new Thread(r1).start();
+        new Thread(r2).start();
+    }
+}
+
+
+class Interviewer implements Runnable {
+    BlockingQueue<String> queue;
+
+    public Interviewer(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("10个候选人都来了");
+        for (int i = 0; i < 10; i++) {
+            String candidate = "Candidate" + i;
+            try {
+                queue.put(candidate);
+                System.out.println("安排好了" + candidate);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            queue.put("stop");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    BlockingQueue<String> queue;
+
+    public Consumer(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String msg;
+        try {
+            while (!(msg = queue.take()).equals("stop")) {
+                System.out.println(msg + "到了");
+            }
+            System.out.println("所有候选人都结束了");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
