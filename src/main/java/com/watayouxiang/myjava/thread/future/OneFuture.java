40f783f8c0a85f87bd01e62b29186168b992bfef
watayouxiang <watayouxiang@qq.com>
2023-03-14 20:20:28 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/future/OneFuture.java
@@ -0,0 +1,33 @@
+package com.watayouxiang.myjava.thread.future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * <p> author：wangtao
+ * <p> email：watayouixang@qq.com
+ * <p> time：2023/3/14
+ * <p> description：演示一个Future的使用方法
+ */
+public class OneFuture {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        Future<Integer> future = service.submit(new CallableTask());
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        service.shutdown();
+    }
+
+    static class CallableTask implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        }
+    }
+}
---
+++ src/main/java/com/watayouxiang/myjava/thread/future/OneFutureLambda.java
@@ -0,0 +1,29 @@
+package com.watayouxiang.myjava.thread.future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * <p> author：wangtao
+ * <p> email：watayouixang@qq.com
+ * <p> time：2023/3/14
+ * <p> description：演示一个Future的使用方法，lambda形式
+ */
+public class OneFutureLambda {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        Callable<Integer> callable = () -> {
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        };
+        Future<Integer> future = service.submit(callable);
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        service.shutdown();
+    }
+}
