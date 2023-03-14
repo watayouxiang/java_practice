8cbe68f3cdba01b9008e0ef53527616e3f9b85b1
watayouxiang <watayouxiang@qq.com>
2023-03-14 20:12:18 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/future/RunnableCantThrowsException.java
@@ -0,0 +1,13 @@
+package com.watayouxiang.myjava.thread.future;
+
+/**
+ * <p> author：wangtao
+ * <p> email：watayouixang@qq.com
+ * <p> time：2023/3/14
+ * <p> description：
+ * Runnable的缺陷：
+ * 1、不能返回一个返回值
+ * 2、也不能抛出 checked Exception
+ */
+public class RunnableCantThrowsException {
+}
