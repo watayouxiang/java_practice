d115a97d9d42444b4dd1a63e9096948ec691f4e5
watayouxiang <watayouxiang@qq.com>
2023-03-14 21:12:55 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/future/FutureTaskDemo.java
@@ -0,0 +1,42 @@
+package com.watayouxiang.myjava.thread.future;
+
+import java.util.concurrent.*;
+
+/**
+ * <p> author：wangtao
+ * <p> email：watayouixang@qq.com
+ * <p> time：2023/3/14
+ * <p> description：演示FutureTask的用法
+ */
+public class FutureTaskDemo {
+    public static void main(String[] args) {
+        Task task = new Task();
+        FutureTask<Integer> futureTask = new FutureTask<>(task);
+
+//        new Thread(futureTask).start();
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(futureTask);
+
+        try {
+            Integer integer = futureTask.get();
+            System.out.println("task运行结果：" + integer);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Task implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("子线程正在计算");
+        Thread.sleep(3000);
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
