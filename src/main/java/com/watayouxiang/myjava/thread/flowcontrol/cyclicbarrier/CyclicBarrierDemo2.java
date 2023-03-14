88b7d56e2556eb16ff97dec684ce00c1eee05726
watayouxiang <watayouxiang@qq.com>
2023-03-14 18:12:13 +0800

    .

---
+++ src/main/java/com/watayouxiang/myjava/thread/flowcontrol/cyclicbarrier/CyclicBarrierDemo2.java
@@ -0,0 +1,49 @@
+package com.watayouxiang.myjava.thread.flowcontrol.cyclicbarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * <p> author：wangtao
+ * <p> email：watayouixang@qq.com
+ * <p> time：2023/3/14
+ * <p> description：演示CyclicBarrier：10个人都到达集合地点后，5人为一组，一组一组出发。
+ */
+public class CyclicBarrierDemo2 {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("所有人都到场了，大家统一出发！");
+            }
+        });
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Task(i, cyclicBarrier)).start();
+        }
+    }
+
+    static class Task implements Runnable {
+
+        private int id;
+        private CyclicBarrier cyclicBarrier;
+
+        public Task(int id, CyclicBarrier cyclicBarrier) {
+            this.id = id;
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程" + id + "现在前往集合地点");
+            try {
+                Thread.sleep((long) (Math.random() * 1000));
+                System.out.println("线程" + id + "到达了集合地点，开始等待其他人到达");
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
