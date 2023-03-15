b58956de128d50825339b51aec564d0db81fb904
watayouxiang <watayouxiang@qq.com>
2023-03-15 15:51:08 +0800

    .

--- myjava/src/main/java/com/watayouxiang/myjava/thread/mycache/computable/ExpensiveFunction.java
+++ myjava/src/main/java/com/watayouxiang/myjava/thread/mycache/computable/ExpensiveFunction.java
@@ -4,7 +4,7 @@ package com.watayouxiang.myjava.thread.mycache.computable;
  * <p> author：wangtao
  * <p> email：watayouixang@qq.com
  * <p> time：2023/3/14
- * <p> description：耗时计算的实现类，实现了Computable接口，但是本身不具备缓存能力，不需要考虑缓存的事情。
+ * <p> description：耗时计算的实现类，但是本身不具备缓存能力，不需要考虑缓存的事情。
  */
 public class ExpensiveFunction implements Computable<String, Integer> {
     @Override
